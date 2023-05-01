b4db42b12926a24fec201e2ca3c3de8e76275730
louis@x502ca <louis@x502ca>
2023-05-02 03:17:11 +0800

    upgrade log_lasb to v2

--- logscripts/log_lastb_ip.sh
+++ logscripts/log_lastb_ip.sh
@@ -1,3 +1,19 @@
-sudo lastb | grep "$(date "+%b %d")"|awk '{a[$3]++} END {for (i in a){if(a[i]>10) print i,":",a[i]|"sort -r -g +2"}}'
+#!/bin/bash 
+if  [[ $1 =~ ^[0-9]+$ ]]
+then 
+  filter1=$1
+else
+  filter1=10
+fi
 
+printf 'Display failed to login > %s times IP in this month (%s)\n' "$filter1" "$(date "+%Y-%m")"
+lastblog="$(sudo lastb -s "$(date "+%Y-%m-01")" -t now)"
+
+# use for check lastlog
+# grep "ubuntu" <<<"$lastblog"|head -10
+
+awk -v filter2="$filter1" '{a[$3]++} END {for (i in a){if(a[i]>filter2) print i,":",a[i]|"sort -r -g +2"}}' <<<"$lastblog"
+
+# v1 one line to filter ip display greate than 10 times
+# sudo lastb | grep "$(date "+%b %d")"|awk '{a[$3]++} END {for (i in a){if(a[i]>10) print i,":",a[i]|"sort -r -g +2"}}'
 
