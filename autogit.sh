521552dfcedf753cf0705cce3f9b50f3ec66ff4e
louis@x502ca <louis@x502ca>
2024-12-21 21:29:11 +0800

    update autogit

--- autogit.sh
+++ autogit.sh
@@ -1,6 +1,10 @@
 cd ~/bash_shell/
 git pull
 echo "Commit made on $(date)" >> commit-log-$(date +'%Y%m%d').txt
+LAST_30_DAYS=$(date -d '30 days ago' --utc +%Y-%m-%d)
+COMMIT_COUNT=$(git rev-list --count --after="$LAST_30_DAYS" HEAD)
+echo "Commit Count in Last 30 Days: $COMMIT_COUNT" >> commit-log-$(date +'%Y%m%d').txt
 git add commit-log-$(date +'%Y%m%d').txt
+
 git commit -m "x502c bash shell Automated commit on $(date)"
 git push
